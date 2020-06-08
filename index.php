f35853ed895e5db997b62d57a6ce36551b528106
=luizmoitinho <luizcarlos_costam@hotmail.com>
2020-06-08 20:11:15 -0300

    usando o verbo get

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
 require_once "vendor/autoload.php";
 $app = new \Slim\App();
 
-$app->get('/', function($request, $response, $args){
-  return $response->getBody()->write('Bem-vindo ao Slim');
+$app->get('/users/[{nome}]', function(Request $request,Response $response,array $args){
+
+  $limit=  $request->getQueryParams()['limit'] ?? 1;
+  $nome =  $args['nome'] ?? '';
+
+  return $response->getBody()->write("{$limit} Usuario autenticado: $nome");
+
 });
 
 $app->run();
