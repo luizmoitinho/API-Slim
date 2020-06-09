bf227968828ae9c0558d0bfbb3d95d19cc504d8a
=luizmoitinho <luizcarlos_costam@hotmail.com>
2020-06-09 14:36:28 -0300

    Verbo post com injeção de dependência

--- index.php
+++ index.php
@@ -6,13 +6,29 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 require_once "vendor/autoload.php";
 $app = new \Slim\App();
 
-$app->get('/users/[{nome}]', function(Request $request,Response $response,array $args){
 
+$configuration = [
+  'settings' => [
+      'displayErrorDetails' => true
+  ]
+];
+$configuration = new \Slim\Container($configuration);
+
+$app->get('/users/[{nome}]', function(Request $request,Response $response,array $args){
   $limit=  $request->getQueryParams()['limit'] ?? 1;
   $nome =  $args['nome'] ?? '';
-
   return $response->getBody()->write("{$limit} Usuario autenticado: $nome");
+});
 
+$app->post('/login',function(Request $request, Response $response, array $args): Response{
+  $data =  $request->getParsedBody();
+  if(is_null($data['matricula']) or empty($data['matricula']) or is_null($data['senha']) or empty($data['senha'])){
+    print('Preencha todos os campos!');
+    exit;
+  }
+  $data =  json_encode($data);
+  $response->getBody()->write("$data");
+  return $response;
 });
 
 $app->run();
