0e8448fd381671516d20653fee76a154992cb01a
=luizmoitinho <luizcarlos_costam@hotmail.com>
2020-06-09 14:48:02 -0300

    Criação de middleware e groups

--- index.php
+++ index.php
@@ -12,7 +12,20 @@ $configuration = [
       'displayErrorDetails' => true
   ]
 ];
+
+$mid01 =  function (Request $request, Response $response, $next): Response{
+  $response->getBody()->write('Dentro do mid01<br>');
+  $response = $next($request,$response);
+  $response->getBody()->write('Dentro do mid02<br>');
+  return $response;
+};
+
 $configuration = new \Slim\Container($configuration);
+
+
+$app->group('/login', function() use($app){
+  $app->get('usuarios');
+})->add($mid01);
 
 $app->get('/users/[{nome}]', function(Request $request,Response $response,array $args){
   $limit=  $request->getQueryParams()['limit'] ?? 1;
@@ -29,6 +42,6 @@ $app->post('/login',function(Request $request, Response $response, array $args):
   $data =  json_encode($data);
   $response->getBody()->write("$data");
   return $response;
-});
+})->add($mid01);
 
 $app->run();
