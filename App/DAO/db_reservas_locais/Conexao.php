6f64b579a6061958c2572e2674c6cc33def46168
isLuizMoitinho <luizcarlos_costam@hotmail.com>
2020-06-23 10:37:29 -0300

    Merge and default connection

--- App/DAO/db_reservas_locais/Conexao.php
+++ App/DAO/db_reservas_locais/Conexao.php
@@ -11,11 +11,11 @@ abstract class Conexao{
   public function __construct(){
     try{
 
-      $host = getenv('DB_RESERVAS_LOCAIS_MYSQL_HOST');
-      $port = getenv('DB_RESERVAS_LOCAIS_MYSQL_PORT');
-      $user = getenv('DB_RESERVAS_LOCAIS_MYSQL_USER');
-      $pass = getenv('DB_RESERVAS_LOCAIS_MYSQL_PASSWORD');
-      $dbname = getenv('DB_RESERVAS_LOCAIS_MYSQL_DBNAME');
+      $host = getenv('DB_RESERVA_LOCAIS_MYSQL_HOST');
+      $port = getenv('DB_RESERVA_LOCAIS_MYSQL_PORT');
+      $user = getenv('DB_RESERVA_LOCAIS_MYSQL_USER');
+      $pass = getenv('DB_RESERVA_LOCAIS_MYSQL_PASSWORD');
+      $dbname = getenv('DB_RESERVA_LOCAIS_MYSQL_DBNAME');
 
       $dsn = "mysql:host={$host};dbname={$dbname};port={$port};";
       $this->pdo = new \PDO($dsn,$user,$pass);
